3422ab6b1cf7be7ec5994c1498ad970c407f1d11
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-02-29 21:26:44 +0200

    Class Book

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,14 @@
-//
-// Created by User on 29.02.2024.
-//
-
 #include "Book.h"
+
+Book::Book()
+        : Book{"None", "None", 0} {}
+
+Book::Book(std::string new_name)
+        : Book{new_name, "None", 0} {}
+
+Book::Book(std::string new_name, std::string new_author)
+        : Book{new_name, new_author, 0} {}
+
+Book::Book(std::string new_name, std::string new_author, int new_yOfW)
+        : name(new_name), author(new_author), yearsOfWriting(new_yOfW) {}
+
