bc76b9f56508c25c9de23459a467ca6eab4d8ff4
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-02-29 22:51:31 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-
+#include "Book.h"
+#include "Library.h"
 int main() {
-
+    Book book_1 ("The Surgeon", "Tess Gerritsen", 2001);
+    book_1.displayInfo();
+    Library library_1 ("Nezalezhnosti Ave", "Bookclub");
+    library_1.displayIn();
     return 0;
 }
