6a809424bb7035f4201fa196b1d461b933dc910c
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-02-29 21:08:43 +0200

    Class Book

---
+++ Book.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 29.02.2024.
+//
+
+#include "Book.h"
---
+++ Book.h
@@ -0,0 +1,22 @@
+#ifndef BOOK_LIB_MANAG_BOOK_H
+#define BOOK_LIB_MANAG_BOOK_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Book {
+private:
+    string name;
+    string author;
+    int yearsOfWriting;
+public:
+    Book();
+    Book(string new_name);
+    Book(string new_name, string new_author);
+    Book(string new_name, string new_author, int new_yOfW);
+    ~Book();
+};
+
+#endif
