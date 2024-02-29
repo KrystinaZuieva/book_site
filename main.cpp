b5916cadbefe1f6b8a07a005f269c3a0c0c45e70
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-02-29 23:25:18 +0200

    The main branch

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "Book.h"
 #include "Library.h"
+#include "Reader.h"
+
 int main() {
-    Book book_1 ("The Surgeon", "Tess Gerritsen", 2001);
+    Book book_1("The Surgeon", "Tess Gerritsen", 2001);
     book_1.displayInfo();
-    Library library_1 ("Nezalezhnosti Ave", "Bookclub");
+    Library library_1("Nezalezhnosti Ave", "Bookclub");
     library_1.displayIn();
+    Reader reader_1("Krystina", "Zuieva", 17);
+    reader_1.displayInf();
     return 0;
 }
