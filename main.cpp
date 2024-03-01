33badb3399a76eb1bc1d91e485fdc17f67221f00
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-01 10:21:09 +0200

    Call all tree classes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Book.h"
-#include "Library.h"
+#include "Freelib.h"
 #include "Reader.h"
 
 int main() {
     Book book_1("The Surgeon", "Tess Gerritsen", 2001);
     book_1.displayInfo();
-    Library library_1("Nezalezhnosti Ave", "Bookclub");
-    library_1.displayIn();
+    Freelib freelib_1("Nezalezhnosti Ave", "Bookclub");
+    freelib_1.displayIn();
     Reader reader_1("Krystina", "Zuieva", 17);
     reader_1.displayInf();
     return 0;
