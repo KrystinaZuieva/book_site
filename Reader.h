3e2631942c772b864899ada401a34cbb08b1dc01
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-01 10:38:17 +0200

    Create method displayInf in Reader

--- Reader.h
+++ Reader.h
@@ -12,16 +12,19 @@ private:
     string name;
     string surname;
     int age;
+    string password;
 public:
     Reader();
     Reader(string new_name);
     Reader(string new_name, string new_sur);
     Reader(string new_name, string new_sur, int new_age);
+    Reader(string new_name, string new_sur, int new_age, string new_pass);
    ~Reader() {};
    void displayInf() {
        cout << "Name: " << name << endl
        << "Surname: " << surname << endl
-       << "Age: " << age << endl;
+       << "Age: " << age << endl
+       << "Password: " << password << endl;
    }
 };
 
