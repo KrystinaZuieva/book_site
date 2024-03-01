6ec24f12482b2c23cb703ceabaa24b1e4cbbf91c
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-01 10:37:17 +0200

    Call all tree classes

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include "Reader.h"
 
 int main() {
-    Book book_1("The Surgeon", "Tess Gerritsen", 2001);
+    Book book_1("The Surgeon", "Tess Gerritsen", 2001, "Crime");
     book_1.displayInfo();
     Freelib freelib_1("Nezalezhnosti Ave", "Bookclub");
     freelib_1.displayIn();
-    Reader reader_1("Krystina", "Zuieva", 17);
+    Reader reader_1("Krystina", "Zuieva", 17, "qwerty");
     reader_1.displayInf();
     return 0;
 }
