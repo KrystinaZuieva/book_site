f2729fe8702a94331a788e157003dfdf025760bc
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-01 10:39:50 +0200

    Announced the body in Book

--- Book.cpp
+++ Book.cpp
@@ -1,14 +1,17 @@
 #include "Book.h"
 
 Book::Book()
-        : Book{"None", "None", 0} {}
+        : Book{"None", "None", 0, "None"} {}
 
 Book::Book(std::string new_name)
-        : Book{new_name, "None", 0} {}
+        : Book{new_name, "None", 0, "None"} {}
 
 Book::Book(std::string new_name, std::string new_author)
-        : Book{new_name, new_author, 0} {}
+        : Book{new_name, new_author, 0, "None"} {}
 
 Book::Book(std::string new_name, std::string new_author, int new_yOfW)
-        : name(new_name), author(new_author), yearsOfWriting(new_yOfW) {}
+        :Book{new_name, new_author, new_yOfW, "None"} {}
+
+Book::Book(std::string new_name, std::string new_author, int new_yOfW, std::string new_gen)
+        : name(new_name), author(new_author), yearsOfWriting(new_yOfW), genre(new_gen) {}
 
