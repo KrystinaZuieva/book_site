91c990ebf2b1548527c321b3529bea463e9e2cd8
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-01 10:40:10 +0200

    Announced the body in Reader

--- Reader.cpp
+++ Reader.cpp
@@ -1,13 +1,16 @@
 #include "Reader.h"
 
 Reader::Reader()
-        : Reader{"None", "None", 0} {}
+        : Reader{"None", "None", 0, "None"} {}
 
 Reader::Reader(std::string new_name)
-        : Reader{new_name, "None", 0} {}
+        : Reader{new_name, "None", 0, "None"} {}
 
 Reader::Reader(std::string new_name, std::string new_sur)
-        : Reader{new_name, new_sur, 0} {}
+        : Reader{new_name, new_sur, 0, "None"} {}
 
 Reader::Reader(std::string new_name, std::string new_sur, int new_age)
-        : name(new_name), surname(new_sur), age(new_age) {}
+: Reader {new_name, new_sur, new_age, "None"} {}
+
+Reader::Reader(std::string new_name, std::string new_sur, int new_age, std::string new_pass)
+        : name(new_name), surname(new_sur), age(new_age), password(new_pass) {}
