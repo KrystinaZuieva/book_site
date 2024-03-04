9bda2f9a81793ce525e05497da12cdb177fc1265
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-05 00:42:56 +0200

    Announced method displayInfo in Book

--- Book.cpp
+++ Book.cpp
@@ -9,9 +9,9 @@ Book::Book(std::string new_name)
 Book::Book(std::string new_name, std::string new_author)
         : Book{new_name, new_author, 0, "None"} {}
 
-Book::Book(std::string new_name, std::string new_author, int new_yOfW)
-        :Book{new_name, new_author, new_yOfW, "None"} {}
+Book::Book(std::string new_name, std::string new_author, int new_yOfP)
+        : Book{new_name, new_author, new_yOfP, "None"} {}
 
-Book::Book(std::string new_name, std::string new_author, int new_yOfW, std::string new_gen)
-        : name(new_name), author(new_author), yearsOfWriting(new_yOfW), genre(new_gen) {}
+Book::Book(std::string new_name, std::string new_author, int new_yOfP, std::string new_gen)
+        : name(new_name), author(new_author), yearsOfPublication(new_yOfP), genre(new_gen) {}
 
