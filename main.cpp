6cdf026eef8a495ede073095cc908df69df2f352
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-05 00:44:23 +0200

    Call all three classes

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include "Book.h"
 #include "Freelib.h"
-#include "Reader.h"
+#include "User.h"
 
 int main() {
+    User user_1("kriistiiinnaa", "kristina13zk@gmail.com", 17);
+    user_1.displayInfo();
     Book book_1("The Surgeon", "Tess Gerritsen", 2001, "Crime");
     book_1.displayInfo();
-    Freelib freelib_1("Nezalezhnosti Ave", "Bookclub");
-    freelib_1.displayIn();
-    Reader reader_1("Krystina", "Zuieva", 17, "qwerty");
-    reader_1.displayInf();
+    Freelib freelib;
+    freelib.addText("According to her,she was raped in May,and killed in July.");
+    freelib.addText("What a beautiful world for women,isn't it?");
+    //freelib.clearText();
+    freelib.readingText();
     return 0;
 }
