1d67b9dcd7629c864afabd422e69fa2f7b554c61
KrystinaZuieva <158556571+KrystinaZuieva@users.noreply.github.com>
2024-03-05 00:47:26 +0200

    Announced text in Freelib

--- Freelib.cpp
+++ Freelib.cpp
@@ -1,10 +1,7 @@
 #include "Freelib.h"
 
 Freelib::Freelib()
-        : Freelib{"None", "None"} {}
+        : Freelib{""} {}
 
-Freelib::Freelib(std::string new_add)
-        : Freelib{new_add, "None"} {}
-
-Freelib::Freelib(std::string new_add, std::string new_name)
-        : address(new_add), name(new_name) {}
+Freelib::Freelib(std::string new_text)
+        : text(new_text) {}
